c3b4c537ecf295b8a61a3e5bc79cb286a198f25e
momosaidi768-prog <momosaidi768@gmail.com>
2026-05-22 22:40:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,18 @@
 import asyncio
-import aiohttp
-from playwright.async_api import async_playwright
 
-TOKEN = "PUT_YOUR_BOT_TOKEN"
-ADMIN_ID = 6675176280
-TG_URL = f"https://api.telegram.org/bot{TOKEN}"
-
-running = True
-
-
-async def send(msg):
-    async with aiohttp.ClientSession() as s:
-        await s.post(f"{TG_URL}/sendMessage", data={
-            "chat_id": ADMIN_ID,
-            "text": msg
-        })
-
-
-async def worker():
-    try:
-        async with async_playwright() as p:
-            print("🚀 Browser starting...")
-
-            browser = await p.chromium.launch(
-                headless=True,
-                args=["--no-sandbox"]
-            )
-
-            page = await browser.new_page()
-
-            print("✅ Browser started")
-
-            while running:
-                print("🔁 Bot running...")
-                await asyncio.sleep(5)
-
-    except Exception as e:
-        print("WORKER ERROR:", e)
-
+print("🔥 BOT STARTING...")
 
 async def main():
-    print("🔥 BOT STARTING...")
 
-    await send("🤖 BOT ONLINE")
-
-    task = asyncio.create_task(worker())
+    print("🤖 BOT RUNNING")
 
     while True:
-        await asyncio.sleep(10)
+        try:
+            print("🔁 alive")
+            await asyncio.sleep(10)
 
+        except Exception as e:
+            print("ERROR:", e)
 
 if __name__ == "__main__":
     asyncio.run(main())
