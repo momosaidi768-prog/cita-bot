19bd1bad5ec829ead28901cdbe5847fd9be74ea4
momosaidi768-prog <momosaidi768@gmail.com>
2026-05-22 04:45:27 +0200

    Update main.py
    Djj

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+print("TOKEN =", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت خدام ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-
-print("Bot started...")
 app.run_polling()
