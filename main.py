ba791c3cde43abdc2a53279aa5dc01ec68859a6d
momosaidi768-prog <momosaidi768@gmail.com>
2026-05-22 04:49:44 +0200

    Update main.py
    Hh

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# قراءة التوكن من Railway Variables
 TOKEN = os.getenv("BOT_TOKEN")
-print("TOKEN =", TOKEN)
+
+# فحص التوكن
+print("TOKEN:", repr(TOKEN))
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN غير موجود في Railway Variables!")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت خدام ✅")
 
-app = ApplicationBuilder().token(TOKEN).build()
+# إنشاء البوت
+app = ApplicationBuilder().token(TOKEN.strip()).build()
+
+# الأوامر
 app.add_handler(CommandHandler("start", start))
+
+print("Bot started...")
+
+# تشغيل البوت
 app.run_polling()
